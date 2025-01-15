200dabca2abfd5da46d7722b56393b18b90e6075
Wilkins Ang <angweijian2001@gmail.com>
2025-01-15 10:49:38 +0800

    Minor Changes

---
+++ src/db/schema/purchases.ts
@@ -0,0 +1,18 @@
+import { sql } from "drizzle-orm";
+import {integer, pgTable, varchar, check, boolean} from "drizzle-orm/pg-core";
+import {timestamps} from "./columns.helpers";
+import {usersTable} from "./users";
+import {productTable} from "./products";
+
+export const purchaseTable = pgTable('purchases', {
+        id: integer().primaryKey().generatedAlwaysAsIdentity(),
+        buyerId: integer().notNull().references(() => usersTable.id),
+        productId: integer().notNull().references(() => productTable.id),
+        quantity: integer().notNull(),
+        isApproved: boolean().notNull().default(false),
+        approvedBy: integer().references(() => usersTable.id),
+        ...timestamps
+    },
+    (table) => [{
+        checkConstraint: check("quantity_check", sql`${table.quantity} > 0`)
+    }]);
