3e50efb83fdfc4054fbf6b191ec1994cf7da7e00
GabrielCWT <77312579+GabrielCWT@users.noreply.github.com>
2025-01-15 10:40:57 +0800

    Update users schema

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -1,9 +1,21 @@
-import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
+import { boolean, check, integer, pgTable, varchar } from 'drizzle-orm/pg-core';
 import { timestamps } from './columns.helpers';
+import { sql } from 'drizzle-orm';
 
-export const usersTable = pgTable('users', {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    name: varchar({ length: 255 }).notNull(),
-    email: varchar({ length: 255 }).notNull().unique(),
-    ...timestamps,
-});
+export const usersTable = pgTable(
+    'users',
+    {
+        id: integer().primaryKey().generatedAlwaysAsIdentity(),
+        name: varchar({ length: 255 }).notNull(),
+        email: varchar({ length: 255 }),
+        password: varchar({ length: 255 }).notNull(),
+        isAdmin: boolean().default(false),
+        credits: integer().default(0).notNull(),
+        ...timestamps,
+    },
+    (table) => [
+        {
+            checkContraints: check('credit_check', sql`${table.credits} >= 0`),
+        },
+    ]
+);
