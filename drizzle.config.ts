057321896d409de9cf9fb09cf1e9ba1d59dded40
GabrielCWT <77312579+GabrielCWT@users.noreply.github.com>
2025-01-15 09:51:58 +0800

    Add drizzle ORM

---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+    out: './drizzle',
+    schema: './src/db/schema.ts',
+    dialect: 'postgresql',
+    dbCredentials: {
+        user: process.env.DB_USER!,
+        password: process.env.DB_PASSWORD!,
+        host: process.env.DB_HOST!,
+        port: Number.parseInt(process.env.DB_PORT!),
+        database: process.env.DB_NAME!,
+        ssl: false, // Set to true if using SSL
+    },
+});
---
+++ src/db/index.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
+
+const user = process.env.DB_USER!;
+const password = process.env.DB_PASSWORD!;
+const host = process.env.DB_HOST!;
+const port = process.env.DB_PORT!;
+const dbName = process.env.DB_NAME!;
+const db = drizzle(`postgres://${user}:${password}@${host}:${port}/${dbName}`);
