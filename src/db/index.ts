535c28aeff1cb16322cd24a428b2090fa82755f7
GabrielCWT <77312579+GabrielCWT@users.noreply.github.com>
2025-01-15 11:33:37 +0800

    Update db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,13 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
+import * as schema from './schema/schema';
 
 const user = process.env.DB_USER!;
 const password = process.env.DB_PASSWORD!;
 const host = process.env.DB_HOST!;
 const port = process.env.DB_PORT!;
 const dbName = process.env.DB_NAME!;
-const db = drizzle(`postgres://${user}:${password}@${host}:${port}/${dbName}`);
+export const db = drizzle({
+    connection: `postgres://${user}:${password}@${host}:${port}/${dbName}`,
+    schema,
+});
