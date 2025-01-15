e79562e0bbe55b51ea6c4cb00ab4c140ff3b35ef
GabrielCWT <77312579+GabrielCWT@users.noreply.github.com>
2025-01-15 10:41:05 +0800

    Implement voucher requests schema

---
+++ src/db/schema/voucherRequests.ts
@@ -0,0 +1,15 @@
+import { integer, pgTable, timestamp, varchar } from 'drizzle-orm/pg-core';
+import { timestamps } from './columns.helpers';
+import { usersTable } from './users';
+
+export const vourcherRequestsTable = pgTable('voucher_requests', {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    userId: integer()
+        .notNull()
+        .references(() => usersTable.id),
+    amount: integer().notNull(),
+    isApproved: varchar({ length: 255 }).notNull(),
+    approvedAt: timestamp(),
+    approvedBy: integer().references(() => usersTable.id),
+    ...timestamps,
+});
